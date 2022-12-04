d18fcd1323fd5567b8d4153bfa6201ecdf916b0a
Declan Gazil <declan.gazil@ufl.edu>
2022-12-04 12:47:41 -0500

    Added Landing Page and Navbar

---
+++ app/src/main/java/com/example/covertcrisis/InteriorActivity.kt
@@ -0,0 +1,31 @@
+package com.example.covertcrisis
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.covertcrisis.databinding.ActivityCalculatorLoginBinding
+import com.example.covertcrisis.databinding.ActivityInteriorBinding
+import kotlinx.android.synthetic.main.activity_interior.*
+
+class InteriorActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityInteriorBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityInteriorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        bottomNavigationView.menu.getItem(0).setOnMenuItemClickListener{ //home
+            val intent = Intent(this, InteriorActivity::class.java)
+            startActivity(intent)
+            true
+        }
+        bottomNavigationView.menu.getItem(1).setOnMenuItemClickListener{ //home
+            val intent = Intent(this, MessagingLogin::class.java)
+            startActivity(intent)
+            true
+        }
+        //TODO::SET BOBBYS
+    }
+}
--- app/src/main/java/com/example/covertcrisis/LoginActivity.kt
+++ app/src/main/java/com/example/covertcrisis/LoginActivity.kt
@@ -35,7 +35,7 @@ class LoginActivity : AppCompatActivity() {
                     if (it.isSuccessful) {
                         //TODO:: THIS WILL NEED TO CHANGE TO LANDING PAGE OF COVERT CRISIS NOT MAIN
                         Toast.makeText(this, "Signing in!", Toast.LENGTH_SHORT).show()
-                        val intent = Intent(this, MessagingLogin::class.java)
+                        val intent = Intent(this, InteriorActivity::class.java)
                         startActivity(intent)
                     } else { //if there is an error when signing up
                         Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
