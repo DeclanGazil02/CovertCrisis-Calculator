8fc6352005c9f578665a12abeb5645f5d18c3d62
robertcprice <73550613+robertcprice@users.noreply.github.com>
2022-12-05 09:39:15 -0500

    Added demo resource menu and permissions screen

---
+++ app/src/main/java/com/example/covertcrisis/ResourceShareLocActivity.kt
@@ -0,0 +1,34 @@
+package com.example.covertcrisis
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import androidx.viewpager.widget.ViewPager
+import androidx.appcompat.app.AppCompatActivity
+import com.example.covertcrisis.ui.main.SectionsPagerAdapter
+import com.example.covertcrisis.databinding.ActivityResourceBinding
+
+class ResourceShareLocActivity : AppCompatActivity() {
+
+private lateinit var binding: ActivityResourceBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+     binding = ActivityResourceBinding.inflate(layoutInflater)
+     setContentView(binding.root)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = binding.viewPager
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = binding.tabs
+        tabs.setupWithViewPager(viewPager)
+        val fab: FloatingActionButton = binding.fab
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/covertcrisis/resources.kt
@@ -0,0 +1,11 @@
+package com.example.covertcrisis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class resources : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resources)
+    }
+}
